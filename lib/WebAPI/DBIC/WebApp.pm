b986df54619aa9b92b0f2e893bb05f1a66365856
Tim Bunce <Tim.Bunce@pobox.com>
2013-02-12 10:46:04 -0600

    initial top-level links for browser

--- lib/WebAPI/DBIC/WebApp.pm
+++ lib/WebAPI/DBIC/WebApp.pm
@@ -14,6 +14,7 @@ use Plack::App::Path::Router;
 use Path::Class::File;
 use Path::Router;
 use Module::Load;
+use JSON;
 
 use Devel::Dwarn;
 
@@ -26,8 +27,7 @@ my $schema = WebAPI::Schema::Corp->new_default_connect(
     {},
     # connect to yesterdays snapshot because we make edits to the db
     # XXX should really have a better approach for this!
-    #"corp_snapshot_previous",
-    "corp"
+    "corp_snapshot_previous",
 );
 
 
@@ -49,7 +49,7 @@ sub mk_generic_dbic_item_set_route_pair {
 
 my @routes;
 push @routes, mk_generic_dbic_item_set_route_pair(
-    '/person_types' => 'PersonType'
+    'person_types' => 'PersonType'
 );
 
 
@@ -81,6 +81,54 @@ while (my $r = shift @routes) {
         },
     );
 };
+
+if (1) {
+    my %links = (self => { href => "/" } );
+
+    my @resource_links;
+    foreach my $route (@{$router->routes})  {
+        my @parts;
+        my %attr;
+        for my $c (@{ $route->components }) {
+            if ($route->is_component_variable($c)) {
+                my $name = $route->get_component_name($c);
+                push @parts, "{/$name}";
+                $attr{templated} = JSON::true;
+            } else {
+                push @parts, "/$c";
+            }
+        }
+        my $url = join("", @parts);
+        $links{$url} = {
+            href => $url,
+            %attr
+        };
+    }
+    #$links{resource_links} = \@resource_links;
+
+=pod
+  "_links": {
+    "self": { "href": "/orders" },
+    "next": { "href": "/orders?page=2" },
+    "find": { "href": "/orders{?id}", "templated": true },
+    "admin": [
+      { "href": "/admins/2", "title": "Fred" },
+      { "href": "/admins/5", "title": "Kate" }
+    ]
+  },
+=cut
+
+    my $root_data = {
+        _links => \%links,
+    };
+
+    $router->add_route('',
+        target => sub {
+            my $request = shift;
+            [ 200, [], [ JSON->new->pretty->encode($root_data) ] ]
+        },
+    );
+}
 
 # XXX should be moved elsewhere, perhaps to a .psgi file
 use Plack::Builder;
