b74223801460ddd50467fee3a3f158e2cd110427
Mike Francis <ungrim97@gmail.com>
2014-08-04 14:11:57 +0100

    Added new TestSchema class for dual pk where one is a datetime

---
+++ t/lib/TestSchema/Result/Gigs.pm
@@ -0,0 +1,48 @@
+package # Hide from Pause
+    TestSchema::Result::Gig;
+
+use strict;
+use warnings;
+
+use base qw(DBIx::Class::Core);
+
+# A Test schema result class for Gigs at a venue.
+# Designed for testing dual primary keys where one is a
+# datetime
+
+__PACKAGE__->load_components(qw(InflateColumn::DateTime));
+
+__PACKAGE__->table('gigs');
+__PACKAGE__->add_columns(
+    'artistid' => {
+        data_type       => 'Integer',
+        is_foreign_key  => 1,
+        is_numeric      => 1,
+    },
+    'datetime' => {
+        data_type       => 'varchar',
+        size            => 30,
+    },
+);
+
+__PACKAGE__->set_primary_key('artistid'); # Artists can't have more than one gig
+__PACKAGE__->set_primary_ket('datetime'); # Two gigs can't be at the same time.
+
+# SQLite doesn't support the DateTime data type. Thus we can't use
+# InflateColumn::DateTime so we have to do it manually here.
+__PACKAGE__->inflate_column('date' => {
+    'inflate' => sub {
+        my ($db_value, $gig) = @_;
+
+        return DateTime::Format::SQLite->parse_datetime($db_value);
+    },
+    'deflate' => sub {
+        my ($date_datetime, $gig) = @_;
+
+        return "$date_datetime";
+    },
+});
+
+# Relationships
+__PACKAGE__->belongs_to(artist => 'TestSchema::Result::Artist', 'artistid');
+
