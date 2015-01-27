f26656b6f52ca5e83667d4591a37260278acc4a5
J. David Lewis <jdtx@tigerlms.com>
2015-01-27 14:44:14 +0000

    Finalize support for Ember ActiveModel, add corresponding tests.

--- lib/WebAPI/DBIC/Resource/ActiveModel/Role/DBIC.pm
+++ lib/WebAPI/DBIC/Resource/ActiveModel/Role/DBIC.pm
@@ -28,59 +28,103 @@ sub activemodel_type {
 }
 
 
+sub traverse_prefetch {
+    my $self = shift;
+    my $set = shift;
+    my $prefetch = shift;
+    my $rel_fcn = shift;
+    my @rel_fcn_args = @_;
+
+    return unless($prefetch);
+    # warn "Traversing ", Dumper($prefetch), ref($set), ': ', join(', ', @rel_fcn_args), "\n\n";
+
+    if($prefetch && !ref($prefetch)) {
+        $self->$rel_fcn($set, $prefetch, @rel_fcn_args);
+    }
+    elsif(ref($prefetch) eq 'HASH') {
+        # warn "ref(set): ", ref($set), "\n";
+        while(my ($prefetch_key, $prefetch_value) = each(%{$prefetch})) {
+            $self->traverse_prefetch($set, $prefetch_key, $rel_fcn, @rel_fcn_args);
+            my $result_subclass = $set->result_class->relationship_info($prefetch_key)->{class};
+            $self->traverse_prefetch($result_subclass, $prefetch_value, $rel_fcn, @rel_fcn_args);
+        }
+    }
+    elsif(ref($prefetch) eq 'ARRAY') {
+        for my $sub_prefetch(@{$prefetch}) {
+            $self->traverse_prefetch($set, $sub_prefetch, $rel_fcn, @rel_fcn_args);
+        }
+    }
+    else {
+        warn "Unsupported ref(prefetch): " . ref($prefetch);
+    }
+}
+
+
 sub render_activemodel_prefetch_rel {
-    my ($self, $set, $relname, $sub_rel, $top_links, $rel_sets, $item_edit_rel_hooks) = @_;
+    my ($self, $set, $relname, $top_links, $rel_sets, $item_edit_rel_hooks) = @_;
 
-    my $rel_info = $set->result_class->relationship_info($relname);
-    my $result_class = $rel_info->{class}||die "panic";
+    my $parent_class = $set->result_class;
+    my $child_class = $parent_class->relationship_info($relname)->{class} || die "panic";
+    # warn "relname: $relname, Child result class: $child_class, set class: " . (ref($set) || $set);
 
-    my @idcolumns = $result_class->unique_constraint_columns('primary'); # XXX wrong
+    my @idcolumns = $child_class->unique_constraint_columns('primary'); # XXX wrong
     if (@idcolumns > 1) { # eg many-to-many that doesn't have a separate id
-        warn "Result class $result_class has multiple keys (@idcolumns) so relations like $relname won't have links generated.\n"
-            unless our $warn_once->{"$result_class $relname"}++;
+        warn "Child result class $child_class has multiple keys (@idcolumns) so relations like $relname won't have links generated.\n"
+            unless our $warn_once->{"$child_class $relname"}++;
         return;
     }
 
-    my $link_url_templated = $self->get_url_template_for_set_relationship($self->set, $relname);
-    return if not defined $link_url_templated;
+    my $rel_typename = $self->type_namer->type_name_for_result_class($child_class);
 
-    my $rel_typename = $self->type_namer->type_name_for_result_class($rel_info->{class});
+    return if $item_edit_rel_hooks->{$parent_class}->{$relname};
 
-    die "panic: item_edit_rel_hooks for $relname already defined"
-        if $item_edit_rel_hooks->{$relname};
-
-    $item_edit_rel_hooks->{$relname} = sub {
+    $item_edit_rel_hooks->{$parent_class}->{$relname} = sub {
         my ($activemodel_obj, $row) = @_;
 
         my $subitem = $row->$relname();
 
-        my $related_set = $rel_sets->{$rel_typename} ||= {};
+        my $rel_set = $rel_sets->{$rel_typename} ||= {};
 
-        my $link_keys;
+        my $rel_ids;
         if (not defined $subitem) {
-            $link_keys = undef;
+            $rel_ids = undef;
         }
         elsif ($subitem->isa('DBIx::Class::ResultSet')) { # one-to-many rel
-            $link_keys = [];
+            $rel_ids = [];
             while (my $subrow = $subitem->next) {
                 my $id = $subrow->id;
-                push @$link_keys, $id;
-                $related_set->{$id} = $self->render_item_as_activemodel_hash($subrow); # XXX typename
+                push @$rel_ids, $id;
+                my $rel_object = $self->render_row_as_activemodel_resource_object($subrow, undef, sub {
+                    my ($activemodel_obj, $row) = @_;
+                    $_->($activemodel_obj, $row) for values %{$item_edit_rel_hooks->{$child_class}};
+                });
+                # in case this object has been pulled in before, do what we can
+                # to preserve the existing keys and add to them as appropriate
+                $rel_set->{$id} //= {};
+                $rel_set->{$id} = { %{$rel_object}, %{$rel_set->{$id}} };
             }
         }
         elsif ($subitem->isa('DBIx::Class::Row')) { # one-to-one rel
-            $link_keys = $subitem->id;
-            $related_set->{$subitem->id} = $self->render_item_as_activemodel_hash($subitem); # XXX typename
+            # $rel_ids = $subitem->id;
+            my $rel_object = $self->render_row_as_activemodel_resource_object($subitem, undef, sub {
+                my ($activemodel_obj, $row) = @_;
+                $_->($activemodel_obj, $row) for values %{$item_edit_rel_hooks->{$child_class}};
+            }); # XXX typename
+            # in case this object has been pulled in before, do what we can
+            # to preserve the existing keys and add to them as appropriate
+            $rel_set->{$subitem->id} //= {};
+            $rel_set->{$subitem->id} = { %{$rel_object}, %{$rel_set->{$subitem->id}} };
         }
         else {
             die "panic: don't know how to handle $row $relname value $subitem";
         }
 
-        # TODO: This should use the relationship name, not the type_name
-        $activemodel_obj->{$rel_typename} = $link_keys;
-
-        # TODO: determine the correct way to use the 'links' key
-        $activemodel_obj->{'links'}->{$rel_typename} = "'$link_url_templated'";
+        # XXX This should use the relationship name, singular, suffixed with '_ids'
+        # This only applies to hasMany relationships since belongsTo relationships
+        # will have the ID included in the row data itself.
+        use Lingua::EN::Inflect::Number qw(to_S to_PL);
+        my $relname_id = to_S($relname).'_ids';
+        $activemodel_obj->{$relname_id} = $rel_ids if($rel_ids);
     }
 }
 
@@ -89,24 +133,24 @@ sub render_activemodel_response { # return top-level document hashref
     my ($self) = @_;
 
     my $set = $self->set;
+    my $prefetch = $self->prefetch;
 
     my $top_links = {};
     my $rel_sets = {};
     my $item_edit_rel_hooks = {};
 
-    for my $prefetch (@{$self->prefetch||[]}) {
-        #warn "prefetch $prefetch";
-        next if $self->param('distinct');
-
-        while (my ($relname, $sub_rel) = each %{$prefetch}){
-            #warn "prefetch $prefetch - $relname, $sub_rel";
-            $self->render_activemodel_prefetch_rel($set, $relname, $sub_rel, $top_links, $rel_sets, $item_edit_rel_hooks);
-        }
+    if (scalar(@{$prefetch})) {
+        $self->traverse_prefetch($set, $prefetch, \&render_activemodel_prefetch_rel, $top_links, $rel_sets, $item_edit_rel_hooks)
+    }
+    else {
+        # warn "no prefetch";
     }
 
+
+    my $result_class = $set->result_class;
     my $set_data = $self->render_set_as_array_of_activemodel_resource_objects($set, undef, sub {
         my ($activemodel_obj, $row) = @_;
-        $_->($activemodel_obj, $row) for values %$item_edit_rel_hooks;
+        $_->($activemodel_obj, $row) for values %{$item_edit_rel_hooks->{$result_class}};
     });
 
     # construct top document to return
@@ -116,8 +160,8 @@ sub render_activemodel_response { # return top-level document hashref
     };
 
     if (keys %$top_links) {
-        # $top_doc = { %{$top_links}, %{$top_doc} };
         # TODO: figure out what to do with the top_links
+        # $top_doc = { %{$top_links}, %{$top_doc} };
     }
 
     if (keys %$rel_sets) {
@@ -182,15 +226,23 @@ sub _render_prefetch_activemodel {
 
 sub render_set_as_array_of_activemodel_resource_objects {
     my ($self, $set, $render_method, $edit_hook) = @_;
-    $render_method ||= 'render_item_as_activemodel_hash';
 
     my @activemodel_objs;
     while (my $row = $set->next) {
-        push @activemodel_objs, $self->$render_method($row);
-        $edit_hook->($activemodel_objs[-1], $row) if $edit_hook;
+        push @activemodel_objs, $self->render_row_as_activemodel_resource_object($row, $render_method, $edit_hook);
     }
 
     return \@activemodel_objs;
+}
+
+sub render_row_as_activemodel_resource_object {
+    my ($self, $row, $render_method, $edit_hook) = @_;
+    $render_method ||= 'render_item_as_activemodel_hash';
+
+    my $obj = $self->$render_method($row);
+    $edit_hook->($obj, $row) if $edit_hook;
+
+    return $obj;
 }
 
 
