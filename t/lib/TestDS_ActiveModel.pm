ac880c8af1acecd327257c3c89a15a0a46b6162d
Tim Bunce <Tim.Bunce@pobox.com>
2015-04-11 22:24:31 +0100

    Minor fixup to previous t/lib/TestDS_ActiveModel.pm commit.

--- t/lib/TestDS_ActiveModel.pm
+++ t/lib/TestDS_ActiveModel.pm
@@ -30,6 +30,7 @@ sub has_activemodel_embedded {
     my ($data, $min, $max) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     is ref $data, 'HASH', "data isn't a hash";
+    die "these HAL tests need to be rewritten to match ActiveModel";
     # XXX these HAL tests need to be rewritten to match ActiveModel
     #is ref $data->{_embedded}, 'HASH', "_embedded isn't hash" or diag $data;
     #my $e = $data->{_embedded};
@@ -37,7 +38,7 @@ sub has_activemodel_embedded {
     #    if defined $min;
     #cmp_ok scalar keys %$e, '<=', $max, "set has more than $max attributes"
     #    if defined $max;
-    return $e;
+    #return $e;
 }
 
 
@@ -45,6 +46,7 @@ sub has_activemodel_embedded_list {
     my ($data, $key, $min, $max) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     my $e = has_activemodel_embedded($data);
+    die "these HAL tests need to be rewritten to match ActiveModel";
     my $set = $e->{$key};
     # XXX these HAL tests need to be rewritten to match ActiveModel
     #if (is ref $set, "ARRAY", "_embedded has $key array") {
